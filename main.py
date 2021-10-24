48013fa54b19b0e3f0b011db3c8157b1e102013c
Franklin Guimaraes <franklincassiano@gmail.com>
2021-10-24 13:52:48 -0300

    calc made

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 # Este programa visa calular a distancia em kms para a velocidade da luz
 
-light_velocity = 300000
+light_velocity = 299792458
+
+light_year = float(input('Quantos anos luz: '))
+
+light_velocity = light_velocity * 60
+light_velocity = light_velocity * 60
+light_velocity = light_velocity * 24
+light_velocity = light_velocity * 365
+
+distance = light_velocity * light_year
+
+print(
+    f'A distancia percorrida em {light_year} anos/luz é de {distance} kms')
